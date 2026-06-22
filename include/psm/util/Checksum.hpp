0f970f5630365c222926274366e5e138736c95ff
rangeset <2432647223@qq.com>
2026-06-23 01:08:06 +0800

    fix(crc): repair the lost ~

--- include/psm/util/Checksum.hpp
+++ include/psm/util/Checksum.hpp
@@ -18,7 +18,7 @@ consteval uint32_t crc32_entry(const uint32_t data) {
     for (int i = 0; i < 8; i++) {
         c = (c & 1) ? (c >> 1) ^ 0xEDB88320 : c >> 1;
     }
-    return c;
+    return ~c;
 }
 
 consteval auto make_crc32_table() {
