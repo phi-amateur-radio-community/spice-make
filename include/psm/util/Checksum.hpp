ead34740534dee81a17ce6d62f3c2aaa0903fd55
rangeset <2432647223@qq.com>
2026-06-23 00:10:27 +0800

    feat(checksum): add checksum calculator library

---
+++ include/psm/util/Checksum.hpp
@@ -0,0 +1,9 @@
+// =====================================================================
+// Copyright (c) 2026 Phiarc Team
+// Licensed under the GNU General Public License Version 3.0 or later
+// https://github.com/phi-amateur-radio-community/spice-make
+// =====================================================================
+// Path /include/psm/util/Checksum.hpp
+// Header file of checksum calculator.
+
+#pragma once
---
+++ src/util/Checksum.cpp
@@ -0,0 +1,9 @@
+// =====================================================================
+// Copyright (c) 2026 Phiarc Team
+// Licensed under the GNU General Public License Version 3.0 or later
+// https://github.com/phi-amateur-radio-community/spice-make
+// =====================================================================
+// Path /src/util/Checksum.cpp
+// Checksum calculator
+
+#include <psm/util/Checksum.hpp>
