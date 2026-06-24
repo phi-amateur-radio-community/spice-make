fea0563509a63608b07c595b57fcabf3dd362d6f
rangeset <2432647223@qq.com>
2026-06-25 01:09:18 +0800

    chore(deps): add CLI11

--- cmake/Deps.cmake
+++ cmake/Deps.cmake
@@ -11,5 +11,13 @@ set(ZSTD_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set(ZSTD_BUILD_SHARED OFF CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(zstd)
 
+FetchContent_Declare(
+        CLI11
+        GIT_REPOSITORY https://github.com/CLIUtils/CLI11.git
+        GIT_TAG v2.6.2
+        GIT_SHALLOW TRUE
+)
+FetchContent_MakeAvailable(CLI11)
+
 add_library(SpiceMakeDeps INTERFACE)
-target_link_libraries(SpiceMakeDeps INTERFACE libzstd_static)
+target_link_libraries(SpiceMakeDeps INTERFACE libzstd_static CLI11::CLI11)
